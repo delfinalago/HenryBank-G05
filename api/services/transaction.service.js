e25f312becfd9d8af0d42885cc2cc341dd8faf4d
dgtech369 <lires.matiash@gmail.com>
2020-12-11 20:18:50 -0300

    transaction_service

---
+++ api/services/transaction.service.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const DbService = require("moleculer-db");
+const SqlAdapter = require("moleculer-db-adapter-sequelize");
+const nodemailer = require("nodemailer");
+const axios = require("axios");
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASSWORD,
+	},
+});
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "transaction",
+	// version: 1
+
+	/**
+	 * Mixins
+	 */
+	mixins: [DbService],
+	adapter: new SqlAdapter("veski", "root", "", {
+		host: "127.0.0.1",
+		dialect: "mysql",
+	}),
+	model: {},
+
+	/**
+	 * Settings
+	 */
+	settings: {
+		// Available fields in the responses
+	},
+
+	/**
+	 * Action Hooks
+	 */
+	hooks: {
+		before: {
+			/**
+			 * Register a before hook for the `create` action.
+			 * It sets a default value for the quantity field.
+			 *
+			 * @param {Context} ctx
+			 */
+			create(ctx) {
+				ctx.params.quantity = 0;
+			},
+
+			testear() {
+				console.log("aca toy broder");
+			},
+		},
+	},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		/**
+		 * The "moleculer-db" mixin registers the following actions:
+		 *  - list
+		 *  - find
+		 *  - count
+		 *  - create
+		 *  - insert
+		 *  - update
+		 *  - remove
+		 */
+
+		testear: {
+			rest: "GET /testear",
+
+			async handler() {
+				const test = await this.validateDni("41471665");
+				return test;
+			},
+		},
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		
+	},
+
+	/**
+	 * Fired after database connection establishing.
+	 */
+	async afterConnected() {
+		// await this.adapter.collection.createIndex({ name: 1 });
+	},
+};
