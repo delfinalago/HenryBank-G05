e8a56df150ac9a239989097810e6d07a19a8c057
nahuelcaputto <nahuelmartinc@gmail.com>
2020-12-13 02:13:54 -0300

    newFeature (client/App.js): added export-linking

--- client/App.js
+++ client/App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as Linking from "expo-linking";
 
-export default function App() {
+// Screens
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "../client/screens/Register";
+import Profile from "./screens/Profile";
+import PreRegister from "./screens/preRegister";
+
+const styles = StyleSheet.create({
+  tab: {
+    marginBottom: 15,
+  },
+});
+
+const prefix = Linking.makeUrl("/");
+
+const LoginStack = createStackNavigator();
+function LoginStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! HENRYBANK </Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <LoginStack.Screen name="Login" component={Login} />
+      <LoginStack.Screen name="PreRegister" component={PreRegister} />
+      <LoginStack.Screen name="Register" component={Register} />
+    </LoginStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const RegisterStack = createStackNavigator();
+function RegisterStackScreen() {
+  return (
+    <RegisterStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <RegisterStack.Screen name="Register" component={Register} />
+    </RegisterStack.Navigator>
+  );
+}
+
+const ProfileStack = createStackNavigator();
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <ProfileStack.Screen name="Profile" component={Profile} />
+    </ProfileStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  const linking = {
+    prefixes: [prefix],
+  };
+  return (
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <Tab.Navigator
+        tabOptions={{
+          style: styles.tab,
+        }}
+      >
+        {/* <Tab.Screen name="Home" component={HomeStackScreen} /> */}
+        <Tab.Screen name="Login" component={LoginStackScreen} />
+        <Tab.Screen name="Register" component={RegisterStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
