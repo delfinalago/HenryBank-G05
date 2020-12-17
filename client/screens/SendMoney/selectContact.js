0307e4737622c38cd534a690aad188cd004ba791
nahuelcaputto <nahuelmartinc@gmail.com>
2020-12-17 03:43:42 -0300

    fix (client/screens/sendmoney): changed styles and call to contacts

--- client/screens/SendMoney/selectContact.js
+++ client/screens/SendMoney/selectContact.js
@@ -17,17 +17,22 @@ export default function SelectContact({ navigation }) {
   const [contacts, setContacts] = useState([{}]);
 
   useEffect(() => {
-    const clientID = 1; //Harcodeado, cambiar a llamado a asyncstorage
-    Axios.get(`${API}/api/contacts/allcontacts`, clientID)
-      .then(({ data }) => {
-        if (data.error) {
-          alert(data.error);
-        } else {
-          console.log(data);
-          setContacts(data.values[0]);
-        }
-      })
-      .catch((error) => console.log(error));
+    AsyncStorage.getItem("@localUser").then((data) => {
+      if (data) {
+        let storage = JSON.parse(data);
+        console.log("storage---------", storage.id);
+        Axios.get(`${API}/api/contacts/all `, storage.id)
+          .then(({ data }) => {
+            if (data.error) {
+              alert(data.error);
+            } else {
+              console.log(data);
+              setContacts(data.values[0]);
+            }
+          })
+          .catch((error) => console.log(error));
+      }
+    });
   }, []);
 
   const [selected, setSelected] = useState({
@@ -44,16 +49,29 @@ export default function SelectContact({ navigation }) {
   };
 
   return (
-    <ScrollView>
-      <View>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Text style={styles.textSub}>
+          Seleccioná un contacto para enviarle dinero
+        </Text>
+      </View>
+      <View style={styles.container}>
         {contacts.map((contact, i) => (
           <ListItem key={i} bottomDivider>
             <Avatar source={{ uri: contact.avatar_url }} />
             <ListItem.Content>
-              <ListItem.Title>
-                {contact.first_name + " " + contact.last_name}
-              </ListItem.Title>
-              <ListItem.Subtitle>{contact.subtitle}</ListItem.Subtitle>
+              <ListItem.Title>{contact.alias}</ListItem.Title>
+              <ListItem.Subtitle>
+                <TouchableOpacity
+                  mode="contained"
+                  secureTextEntry={true}
+                  title=""
+                  onPress={() => navigation.navigate("SendMoney")}
+                  style={styles.button}
+                >
+                  <Text>Enviar dinero</Text>
+                </TouchableOpacity>
+              </ListItem.Subtitle>
             </ListItem.Content>
           </ListItem>
         ))}
@@ -62,4 +80,46 @@ export default function SelectContact({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: "#fff",
+  },
+  textTitle: {
+    fontSize: 40,
+  },
+  textSub: {
+    fontSize: 20,
+  },
+  input: {
+    alignItems: "center",
+    width: "auto",
+    height: 50,
+    marginHorizontal: 10,
+    borderWidth: 2,
+    marginTop: 120,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderColor: "#00716F",
+    borderRadius: 10,
+    paddingVertical: 2,
+  },
+  button: {
+    marginHorizontal: 55,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    backgroundColor: "#00716F",
+    paddingVertical: 10,
+    borderRadius: 23,
+  },
+  container: {
+    paddingTop: 100,
+    paddingHorizontal: 30,
+    backgroundColor: "#FFF",
+    height: "100%",
+    flex: 3,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+  },
+});
